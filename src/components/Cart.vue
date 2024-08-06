38430847ff0f48ea86ee8ff06fea673c5fbf2b09
Clément Guillemot <guillemot.clement@protonmail.com>
2024-08-06 08:27:45 -0400

    Ajout des premiers composants de base

---
+++ src/components/Cart.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>Cart</p>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/Shop.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>Shop</p>
+</template>
+
+<style lang="scss"></style>
